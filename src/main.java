3b5e1fceeae8c1c660e795b3ef97abbc3b3ca5d6
Walterb0b <osca0001@stud.ek.dk>
2025-10-07 11:31:02 +0200

    Lidt Stub

--- src/main.java
+++ src/main.java
@@ -1,3 +1,8 @@
 public static void main(String[] args) {
 
 }
+
+//TODO: Brugerinterface
+//TODO: Pizzamenu (ENUMS)
+//TODO: Sort function til at sortere pizza efter afhæntning
+//TODO: Marker ordrer som afhæntet og gem til fil
