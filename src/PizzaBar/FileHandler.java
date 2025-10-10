55f447fdbeae00e933d1fa0468b1a13e1f8f9000
Namirah0310 <nare0001@stud.ek.dk>
2025-10-10 09:43:37 +0200

    added to stub, Namirah Commit05

--- src/PizzaBar/FileHandler.java
+++ src/PizzaBar/FileHandler.java
@@ -1,8 +1,24 @@
 package PizzaBar;
+//TODO: import anything necessary
 
 public class FileHandler {
-    //TODO: variable fileName
-    //TODO: make a createFile() method
-    //TODO: make a writeFile() method
-    //TODO: make a readFile() method
+    //TODO: variable fileName, should be added in the class these methods are called to
+    //TODO: create a FileHandler object in the logic class.
+
+    public void createFile(String filename){
+        //TODO: create a new File object
+        // TODO: make a try catch for creating a new file
+    }
+
+    public void writeFile(String filename) {
+        //TODO: add new FileWriter object. remember to close writer to save changes.
+        //TODO: make a try catch for writing to the file
+    }
+
+    public String readFileAsString(){
+        //TODO: make a new StringBuilder object
+        //TODO: make a try catch with new File and Scanner objects.
+        // remember to close scanner
+        return null; //TODO: return a toString of the StringBuilder object.
+    }
 }
