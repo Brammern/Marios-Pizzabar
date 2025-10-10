1e5b7507affc9ffb90d34007961ff0ef114dc475
Namirah0310 <nare0001@stud.ek.dk>
2025-10-10 10:43:43 +0200

    added to stub, Namirah Commit07

--- src/PizzaBar/OrderManager.java
+++ src/PizzaBar/OrderManager.java
@@ -1,7 +1,7 @@
 package PizzaBar;
 
 public class OrderManager {
-    //TODO: make list of orders
+
     public void createOrder(){
         //TODO: create new Order object based on user input, and add to list
     }
@@ -11,18 +11,21 @@ public class OrderManager {
         //TODO: loop through and print the list of active orders
     }
 
-    public void findOrderById(){
-        //TODO: sort list of active orders by Id
-        //TODO: loop through and print the list of active orders
+    public String findOrderById(){
+        //TODO: select an order Id
+        // loop through the list of orders to find selected order
+        return null; //placeholder return
     }
 
     public void deleteOrder(){
         //TODO: select an order by Id
-        //TODO: remove an order from the list
+        //TODO: remove the selected order from the list
+        // use a try catch to validate if the selected order exists in the list
     }
 
     public void modifyOrder(){
         //TODO: select an order based on Id
+        // use a try catch to validate if the selected order exists in the list
         //TODO: select an attribute to modify
         //TODO: modify the order attribute
     }
