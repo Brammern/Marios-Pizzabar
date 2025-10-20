263a9e65f89a5c8f5439e2d26cb8d0040cf1cf79
BrammerN <k4wkv49@gmail.com>
2025-10-20 14:40:14 +0200

    Removal of old project files

--- src/PizzaBar/FamilySizePizza.java
+++
@@ -1,7 +0,0 @@
-package PizzaBar;
-
-public class FamilySizePizza {
-    //TODO: override price attribute
-    //TODO: make constructor
-    //TODO: override toString()
-}
--- src/PizzaBar/Logic.java
+++
@@ -1,6 +0,0 @@
-package PizzaBar;
-
-public class Logic {
-    //TODO: switch statement to switch between menu card, list of active orders,
-    // add order remove order and show full order history
-}
