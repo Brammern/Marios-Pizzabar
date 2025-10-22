1073a55d65b80e1ae621e19b4a8e561e3709d7ce
Namirah0310 <nare0001@stud.ek.dk>
2025-10-22 11:47:14 +0200

    sort the list, Namirah Commit20

--- src/PizzaBar/CompareByTime.java
+++ src/PizzaBar/CompareByTime.java
@@ -8,6 +8,6 @@ public class CompareByTime implements Comparator<Order> {
 
     @Override
     public int compare(Order o1, Order o2){
-        return Double.compare(o2.minutesToPickup(), o1.minutesToPickup());
+        return Long.compare(o2.minutesToPickup(), o1.minutesToPickup());
     }
 }
